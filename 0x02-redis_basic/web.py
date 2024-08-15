1345e8b8386391ee652e41c7c9ac85e0dd440a58
Ahmad Abdullahi <bayero@Ahmads-MacBook-Pro.local>
2024-08-15 19:38:40 +0100

    Spain

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,43 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-import redis
-import requests
+""" web """
+
 from functools import wraps
 from typing import Callable
+import requests
+import redis
+
+# Initialize Redis client
+r = redis.Redis()
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
-
-
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def count_requests(method: Callable) -> Callable:
+    """Tracks how many times a particular URL was accessed
+    in the key 'count:{url}' and caches the result with an expiration
+    time of 10 seconds.
+    """
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(url):
+        # Increment the access count
+        r.incr(f"count:{url}")
+
+        # Check if the result is already cached
+        cached_html = r.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+
+        # Fetch the content and cache it
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+    return wrapper
 
 
-@data_cacher
+@count_requests
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """Obtains the HTML content of a particular URL and returns it."""
+    req = requests.get(url)
+    return req.text
+
+
+if __name__ == "__main__":
+    get_page('http://google.com')
